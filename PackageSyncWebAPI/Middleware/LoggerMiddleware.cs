bfc1c996d63e932f3745f594518015b52c9e7112
Nikola-Dronjak <nikola.s.dronjak@gmail.com>
2024-12-16 02:58:13 +0100

    feat: Added the LoggerMiddleware class.

---
+++ PackageSyncWebAPI/Middleware/LoggerMiddleware.cs
@@ -0,0 +1,95 @@
+﻿using Serilog;
+using System.Diagnostics;
+using System.Text;
+using ILogger = Serilog.ILogger;
+
+namespace PackageSyncWebAPI.Middleware
+{
+    public class LoggerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger _logger;
+
+        public LoggerMiddleware(RequestDelegate next)
+        {
+            _next = next;
+            _logger = Log.ForContext<LoggerMiddleware>();
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            var request = httpContext.Request;
+            var uri = $"{request.Scheme}://{request.Host}{request.Path}{request.QueryString}";
+
+            if(request.Method == "POST" || request.Method == "PUT")
+            {
+                var requestBody = await new StreamReader(request.Body).ReadToEndAsync();
+                _logger.Information("Request started: {Protocol} {Method} {URI} with request body {RequestBody}.", request.Protocol, request.Method, uri, requestBody);
+                request.Body = new MemoryStream(Encoding.UTF8.GetBytes(requestBody));
+            }
+            else
+            {
+                _logger.Information("Request started: {Protocol} {Method} {URI}.", request.Protocol, request.Method, uri);
+            }
+
+
+
+            var response = httpContext.Response;
+            Stream originalBody = response.Body;
+            try
+            {
+                using var memStream = new MemoryStream();
+                response.Body = memStream;
+
+                var stopwatch = Stopwatch.StartNew();
+                await _next(httpContext);
+                stopwatch.Stop();
+
+                memStream.Position = 0;
+                string responseBody = new StreamReader(memStream).ReadToEnd();
+
+                if (response.StatusCode == 200 || response.StatusCode == 201)
+                {
+                    _logger.Information("Request finished successfully: {Protocol} {Method} {URI} with status {StatusCode} and the following response {ResponseBody} in {EclipsedMiliseconds}ms.",
+                        request.Protocol,
+                        request.Method,
+                        uri,
+                        response.StatusCode,
+                        responseBody,
+                        stopwatch.ElapsedMilliseconds);
+                }
+                else if (response.StatusCode == 400 || response.StatusCode == 401 || response.StatusCode == 404)
+                {
+                    _logger.Warning("Client-side error occured: {Protocol} {Method} {URI} with status {StatusCode} and the following response {ResponseBody} in {EclipsedMiliseconds}ms.",
+                        request.Protocol,
+                        request.Method,
+                        uri,
+                        response.StatusCode,
+                        responseBody,
+                        stopwatch.ElapsedMilliseconds);
+                }
+                else if (response.StatusCode == 500)
+                {
+                    _logger.Error("Server-side error occurred: {Protocol} {Method} {URI} with status {StatusCode} and the following response {ResponseBody} in {ElapsedMilliseconds} ms.",
+                        request.Protocol,
+                        request.Method,
+                        uri,
+                        response.StatusCode,
+                        responseBody,
+                        stopwatch.ElapsedMilliseconds);
+                }
+
+                memStream.Position = 0;
+                await memStream.CopyToAsync(originalBody);
+            }
+            catch (Exception)
+            {
+                throw new Exception("Something went wrong!");
+            }
+            finally
+            {
+                response.Body = originalBody;
+            }
+        }
+    }
+}
--- PackageSyncWebAPI/Program.cs
+++ PackageSyncWebAPI/Program.cs
@@ -1,22 +1,35 @@
-using FluentValidation.AspNetCore;
-using FluentValidation;
 using Microsoft.EntityFrameworkCore;
 using PackageSyncWebAPI.Infrastructure;
 using PackageSyncWebAPI.Services;
+using Serilog;
+using PackageSyncWebAPI.Middleware;
+using FluentValidation;
+using PackageSyncWebAPI.Models;
+using PackageSyncWebAPI.Validators;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-
-builder.Services.AddValidatorsFromAssemblyContaining<Program>();
-builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter());
+    });
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("PackageSyncDb"));
 
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
+
+builder.Services.AddScoped<IValidator<Package>, PackageValidator>();
 builder.Services.AddScoped<IPackageService, PackageService>();
+
+builder.Host.UseSerilog((context, configuration) => configuration.ReadFrom.Configuration(context.Configuration));
 
 var app = builder.Build();
 
@@ -25,6 +38,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<LoggerMiddleware>();
 
 app.UseHttpsRedirection();
 
